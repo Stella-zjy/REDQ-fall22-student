bcaabd379e0ec2b1a607dcd7393a9e9fc9b7a3f8
Stella-zjy <jz3676@nyu.edu>
2022-12-02 01:20:35 +0800

    update

--- plot_utils/plot_proj4.py
+++ plot_utils/plot_proj4.py
@@ -16,28 +16,30 @@ plot_proj4 = True
 if plot_proj4:
     quick_plot(
         [
-         'SAC UTD 1', 'SAC UTD 5'],
+         'SAC UTD 1', 'SAC UTD 5', 'SAC UTD 1 with Reset', 'SAC UTD 5 with Reset'],
         [
             'exp_e300_q2_uf1_lr0.0003_g0.99_p0.995_ss5000_b64_h64_rmFalse',
             'exp_e300_q2_uf5_lr0.0003_g0.99_p0.995_ss5000_b64_h64_rmFalse',
-        ],
-        envs=envs,
-        save_name='SAC_UTD_without_Reset',
-        base_data_folder_path=data_path,
-        y_value=standard_ys
-    )
-
-if plot_proj4:
-    quick_plot(
-        [
-         'SAC UTD 1', 'SAC UTD 5'],
-        [
             'exp_e300_q2_uf1_lr0.0003_g0.99_p0.995_ss5000_b64_h64_rmTrue',
             'exp_e300_q2_uf5_lr0.0003_g0.99_p0.995_ss5000_b64_h64_rmTrue',
         ],
         envs=envs,
-        save_name='SAC_UTD_with_Reset',
+        save_name='SAC_UTD',
         base_data_folder_path=data_path,
         y_value=standard_ys
     )
+
+# if plot_proj4:
+#     quick_plot(
+#         [
+#          'SAC UTD 1', 'SAC UTD 5'],
+#         [
+#             'exp_e300_q2_uf1_lr0.0003_g0.99_p0.995_ss5000_b64_h64_rmTrue',
+#             'exp_e300_q2_uf5_lr0.0003_g0.99_p0.995_ss5000_b64_h64_rmTrue',
+#         ],
+#         envs=envs,
+#         save_name='SAC_UTD_with_Reset',
+#         base_data_folder_path=data_path,
+#         y_value=standard_ys
+#     )
 
